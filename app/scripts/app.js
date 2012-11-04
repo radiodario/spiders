14ba6da4bc9b0450eb3b0847ced27f0ef5aaee52
radiodario <dario.villanueva@gmail.com>
2012-11-04 23:53:54 +0000

    better charts

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,13 @@
 define([], function() {
     // var data = [{a : 1, b: 3, c: 5, d: 3, e:4, f:2}];
-	var data =[[5, 'a'], [3, 'b'], [1,'c'], [2,'d'], [5,'e'], [5,'f'],[4,'g'], [2, 'i']];
+	var data =[[5, 'stamina'], 
+						 [3, 'power'], 
+						 [1,'attack'], 
+						 // [2,'defense'], 
+						 // [5,'energy'], 
+						 // [5,'magic'],
+						 [4,'charisma'], 
+						 [2, 'awesome']];
 
 
 
--- app/scripts/spider.js
+++ app/scripts/spider.js
@@ -1,22 +1,16 @@
 /* a reusable spider chart */
 function spiderChart() {
-	// var data = [{a : 1, b: 3, c: 5, d: 3, e:4, f:2}];
-	//var data = [5, 3, 1, 2, 5, 5,4, 2];
-	var zeros = [0, 0, 0, 0, 0, 0,0, 0];
-	var maxes = [5, 5, 5, 5, 5, 5,5, 5];
-	var measures = ['a', 'b', 'c', 'd', 'e', 'f', 'g', "i"];
-
-	console.log('drawing chart');
-
 
 	// define the chart
-	var margin = 30,
-			height = 400,
-			width = 400,
-			radius = 100,
+	var margin = 60,
+			height = 250,
+			width = 300,
+			radius = (height-margin)/2,
 			ticks = 4, // how many ticks on the grid?
       value = function(d) { return d[0]; },
-      label = function(d) { return d[1]; };
+      label = function(d) { return d[1]; },
+      title = "spider chart",
+      subtitle = "blah";
 	// make some scales
 	var scales = d3.scale.linear()
 	var line = d3.svg.line()
@@ -28,26 +22,28 @@ function spiderChart() {
 	function chart (selection) {
 		selection.each(function(data) {
 
+			maxX = d3.max(data, value) > 0 ? d3.max(data, value) : 1
 			// scale update
-			scales.domain([0, d3.max(data, value)]).range([0,radius]);
+			scales.domain([0, maxX]).range([0,radius]);
 
-			
 			// calculate the positions
 			data = data.map(function(d, i){
 				return {
 					x: calculateX(d, i, data.length),
 					y: calculateY(d, i, data.length),
-					val: d[0],
-					label: d[1]
+					val: value(d),
+					label: label(d)
 				}
 			})
 
-			console.log(data)
+			console.log(data, scales.domain())
 			// select the svg element if it exists
 			var svg = d3.select(this).selectAll("svg").data([data])
 
 			// create the skeleton
 			var gEnter = svg.enter().append("svg").append("g");
+			gEnter.append("text").attr("class", "title label");
+			gEnter.append("text").attr("class", "subtitle label");
 			gEnter.append("g").attr("class", "grid");
 			gEnter.append("g").attr("class", "axes");
 			gEnter.append("path").attr("class", "spider");
@@ -63,8 +59,6 @@ function spiderChart() {
 			// the grid data, number of ticks
 			var gridData = buildAxisGrid(data.length, ticks);
 
-			console.log(gridData)
-
 			// add the grid
 			var gridEnter = g.select('.grid')
 				.selectAll('.gridlevel')
@@ -76,22 +70,39 @@ function spiderChart() {
 	    	.style("opacity", 0.3)
 	    	.attr("d", line);
 
-			// update the paths
-			//g.selectAll('gridLevel')
-				
-
-
-
-
 			// add the axes
 			var ax = g.select("g.axes").selectAll("g.axis")
 				.data(data)
 				.enter().append("g").attr("class", "axis");
 
 			ax.append("svg:text")
-				.text(function(d){ return label(d)})
-				.attr("x", function(d, i) { return Math.sin(angle(i, data.length)) * scales(5);})
-				.attr("y", function(d, i) { return Math.cos(angle(i, data.length)) * scales(5);});
+				.style("text-anchor", function(d, i) {
+						var x = Math.sin(angle(i, data.length)) * scales(scales.domain()[1]);
+						if (Math.abs(x) < 0.1) {
+							return "middle"
+						}
+						if (x > 0) {
+							return "start"
+						}
+
+						return "end"
+				})
+				.attr("dy", function(d, i) {
+						var y = Math.cos(angle(i, data.length)) * scales(scales.domain()[1]);
+						if (Math.abs(y) < 0.1) {
+							return ".72em"
+						}
+						if (y > 0) {
+							return "1em"
+						}
+						return "-.3em"
+				})
+				.style("fill", "#333")
+				.style("font-size", "9pt")
+				.style("opacity", 0.3)
+				.text(function(d){ return d.label})
+				.attr("x", function(d, i) { return Math.sin(angle(i, data.length)) * scales(scales.domain()[1]);})
+				.attr("y", function(d, i) { return Math.cos(angle(i, data.length)) * scales(scales.domain()[1]);});
 
 			ax.append("svg:line")
 			    .style("stroke", "#000")
@@ -101,6 +112,24 @@ function spiderChart() {
 				.attr("y1", function(d, i) { return Math.cos(angle(i, data.length)) * scales(scales.domain()[0]);})
 				.attr("x2", function(d, i) { return Math.sin(angle(i, data.length)) * scales(scales.domain()[1]);})
 				.attr("y2", function(d, i) { return Math.cos(angle(i, data.length)) * scales(scales.domain()[1]);})
+
+			// update the title
+			g.select('.title')
+				.text(function(d) { console.log(title); return title; })
+				.attr("y", height/2)
+				.attr("dy", "-1em")
+				.style("text-anchor", "middle");
+
+			// update the subtitle
+			g.select('.subtitle')
+				.text(subtitle)
+				.style("font-weight", "200")
+				.style("font-size", "9px")
+				.attr("dy", "-0.1em")
+				.attr("y", height/2)
+
+				.style("text-anchor", "middle");
+
 
 			// update the spider line
 			g.select('.spider')
@@ -153,28 +182,71 @@ function spiderChart() {
   // * build the spider axis * //
   // rewrite this to conform to d3 axis style? //
   function buildAxisGrid(length, ticks) {
-  	
   	var min = scales.domain()[0];
-  	var max = scales.domain()[1];
+  	var max = scales.domain()[1] > 0 ? scales.domain()[1] : 1;
   	var increase = max/ticks;
 
+  	console.log("building axis", length, ticks, min, max, increase)
   	gridData = []
   	for (var i = 0; i <= ticks; i++ ) {
   		val = min + i*increase;
-  		var d = [val];
-  		gridpoints = [];
+  		var d = {value:val};
+  		gridPoints = [];
   		for (var j = 0; j <= length; j++) {
-  			gridpoints.push({
+  			gridPoints.push({
 					x: calculateX(d, j, length),
 					y: calculateY(d, j, length),
 				})
   		};
-  		gridData.push(gridpoints)
+  		
+  		gridData.push(gridPoints)
   	}
 
   	return gridData;
 
   }
+
+	chart.margin = function(_) {
+    if (!arguments.length) return margin;
+    margin = _;
+    return chart;
+  };
+
+  chart.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    return chart;
+  };
+
+  chart.height = function(_) {
+    if (!arguments.length) return height;
+    height = _;
+    return chart;
+  };
+
+  chart.value = function(_) {
+    if (!arguments.length) return value;
+    value = _;
+    return chart;
+  };
+
+  chart.label = function(_) {
+    if (!arguments.length) return label;
+    label = _;
+    return chart;
+  };
+
+  chart.title = function(_) {
+    if (!arguments.length) return title;
+    title = _;
+    return chart;
+  };  
+
+  chart.subtitle = function(_) {
+    if (!arguments.length) return subtitle;
+    subtitle = _;
+    return subtitle;
+  };
 
 
   return chart;
