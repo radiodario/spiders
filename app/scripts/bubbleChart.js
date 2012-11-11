2091fe07c05665e304d7c890390e6dec80467b8f
radiodario <dario.villanueva@gmail.com>
2012-11-11 19:10:07 +0000

    added accessors for bubble chart

--- app/scripts/bubbleChart.js
+++ app/scripts/bubbleChart.js
@@ -2,9 +2,9 @@
 
 function bubbleChart() {
 	var margin = {top:10, right:10, bottom:10, left:10};
-	var width  = 500;
+	var width  = 200;
 	var height = 200;
-	var offset = -2;
+	var offset = 0;
 	var value  = function(d) { return d.count };
 	var format = d3.format("n");
 	var colors = d3.scale.category10();
@@ -14,7 +14,6 @@ function bubbleChart() {
 
 	function chart (selection) {
 		selection.each(function(data) {
-
 
 			// prep the data
 			var categories = {
@@ -33,7 +32,7 @@ function bubbleChart() {
             .value(value)
             .padding(1.5);
 
-			var data = bubble.nodes(categories);
+			var data = bubble.nodes(categories).filter(function(d) { return !d.children; });;
 
 			// chart scaffolding
 			var svg = d3.select(this).selectAll("svg").data([data]);
@@ -54,6 +53,9 @@ function bubbleChart() {
 
  			// update chilling points
       g.selectAll('circle')
+          .data(function(d) {return d;})      
+
+      g.selectAll('text.label')
           .data(function(d) {return d;})
 
 
@@ -72,6 +74,7 @@ function bubbleChart() {
 				.attr("class", "label")
 				.attr("text-anchor", "middle")
 				.attr("dy", ".3em")
+				.style("fill", "#fff")
 
 			g.selectAll("g.node")
 				.transition()
@@ -86,13 +89,73 @@ function bubbleChart() {
 				.style("fill", function(d, i) {return colors(i)});
 
 			g.selectAll("text.label")
+				.style("font-size", function(d) { 
+                        var size = d.r/3
+                        // if (size < 11) {
+                        //     return '10pt';
+                        // } else {
+                            return size + 'pt';
+                        // }
+                    })
 				.text(function (d) { return d.name;})
 
 
 		});
 	}
 
+	chart.margin = function(_) {
+    if (!arguments.length) return margin;
+    margin = _;
+    return chart;
+  };
 
+	chart.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    return chart;
+  };
+
+  chart.height = function(_) {
+    if (!arguments.length) return height;
+    height = _;
+    return chart;
+  };
+
+  chart.offset = function(_) {
+    if (!arguments.length) return offset;
+    offset = _;
+    return chart;
+  };
+
+  chart.value = function(_) {
+    if (!arguments.length) return value;
+    value = _;
+    return chart;
+  };
+
+  chart.format = function(_) {
+    if (!arguments.length) return format;
+    format = _;
+    return chart;
+  };
+
+  chart.colors = function(_) {
+    if (!arguments.length) return colors;
+    colors = _;
+    return chart;
+  };
+
+  chart.title = function(_) {
+    if (!arguments.length) return title;
+    title = _;
+    return chart;
+  };
+
+  chart.duration = function(_) {
+    if (!arguments.length) return duration;
+    duration = _;
+    return chart;
+  };
 
 
 
